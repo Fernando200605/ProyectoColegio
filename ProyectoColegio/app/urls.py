03dd4924cc7a300bfa19822fd3b0e8952280e4da
David <ferchitomonroy@gmail.com>
2026-02-02 21:50:12 -0500

    Se agregaron estilos boostrap y ademas se implemento una nueva vista

--- ProyectoColegio/app/urls.py
+++ ProyectoColegio/app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from app.views import *
 app_name = 'app'
 urlpatterns = [
-    path('vista1/',index ,name='vista1')
+    path('vista1/',index ,name='vista1'),
+    path('usuario/',listar_usuario,name="index_usuario"),
 ]
