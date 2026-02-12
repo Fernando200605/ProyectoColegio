05582f919d7c1060a94e4e1a6b88ea679c3dbea8
Nereo Toro <nereotorom@gmail.com>
2026-02-12 06:45:46 -0500

    Se desarrollaron las vistas de Inventario con Categoria, Marca, Tipo de Elemento y Unidad de Medida

--- ProyectoColegio/app/forms.py
+++ ProyectoColegio/app/forms.py
@@ -1,6 +1,10 @@
 from dataclasses import field
 from django import forms
 from app.models import Curso
+from app.models import categoria
+from app.models import Elemento
+from django import forms
+from app.models import marca, tipoelemento, UnidadMedida, Elemento
 
 class CursoForm(forms.ModelForm):
     class Meta:
@@ -29,3 +33,39 @@ class CursoForm(forms.ModelForm):
         if capacidad <= 0:
             raise forms.ValidationError("La capacidad debe ser un numero positivo.")
         return capacidad
+    
+         
+
+
+class marcaForm(forms.ModelForm):
+    class Meta:
+        model = marca
+        fields = '__all__'
+
+class TipoElementoForm(forms.ModelForm):
+    class Meta:
+        model = tipoelemento
+        fields = '__all__'
+
+class UnidadMedidaForm(forms.ModelForm):
+    class Meta:
+        model = UnidadMedida
+        fields = '__all__'
+
+class ElementoForm(forms.ModelForm):
+    class Meta:
+        model = Elemento
+        fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'marca': forms.Select(attrs={'class': 'form-control'}),
+            'tipo': forms.Select(attrs={'class': 'form-control'}),
+            'unidad_medida': forms.Select(attrs={'class': 'form-control'}),
+            'cantidad': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+class CategoriaForm(forms.ModelForm):
+    class Meta:
+        model = categoria
+        fields = ['nombre']
+
---
+++ ProyectoColegio/app/views/Categoria/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, View
+from app.models import categoria
+from app.forms import CategoriaForm
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.db import connection
+
+
+class CategoriaCreateView(CreateView):
+    model = categoria
+    form_class = CategoriaForm
+    template_name = 'Categoria/crear.html'
+    success_url = reverse_lazy('app:index_inventario')  # cambia si tu listado tiene otro nombre
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = "Crear Categoría"
+        context['listar_url'] = reverse_lazy('app:index_inventario')
+        context['btn_name'] = "Guardar"
+        return context
+
+    def form_valid(self, form):
+        messages.success(self.request, "Categoría creada correctamente")
+        return super().form_valid(form)
