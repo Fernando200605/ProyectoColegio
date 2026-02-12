d4c7f8abebbc01d6bf9bc4465c8a3260b524d622
Leidy <leidyxcalderon2910@gmail.com>
2026-02-12 07:12:47 -0500

    Se agrego las vistas de evento,movimiento y notificacion

--- ProyectoColegio/app/forms.py
+++ ProyectoColegio/app/forms.py
@@ -1,10 +1,85 @@
 from dataclasses import field
 from django import forms
 from app.models import Curso
+from app.models import Movimiento
+from app.models import Evento
+from app.models import Notificacion
 
 class CursoForm(forms.ModelForm):
     class Meta:
         model = Curso
+        fields = '__all__'
+        widgets = {
+            'nom': forms.TextInput(attrs={
+                'class':'form-control'
+            }),
+            'jornada':forms.TextInput(attrs={
+                'class':'form-control'
+            }),
+            'codigo':forms.TextInput(attrs={
+                'class':'form-control'
+            }),
+            'capacidad':forms.NumberInput(attrs={
+                'class':'form-control'
+            }),
+            'docenteid':forms.Select(attrs={
+                'class':'form-control'
+            })
+        }
+    # Validación personalizada para el campo capacidad
+    def clean_capacidad(self):
+        capacidad = self.cleaned_data.get('capacidad')
+        if capacidad <= 0:
+            raise forms.ValidationError("La capacidad debe ser un numero positivo.")
+        return capacidad
+    
+class MovimientoForm(forms.ModelForm):
+    class Meta:
+        model = Movimiento
+        fields = '__all__'
+        widgets = {
+            'nom': forms.TextInput(attrs={
+                'class':'form-control'
+            }),
+            'jornada':forms.TextInput(attrs={
+                'class':'form-control'
+            }),
+            'codigo':forms.TextInput(attrs={
+                'class':'form-control'
+            }),
+            'capacidad':forms.NumberInput(attrs={
+                'class':'form-control'
+            }),
+            'docenteid':forms.Select(attrs={
+                'class':'form-control'
+            })
+        }
+    # Validación personalizada para el campo capacidad
+    def clean_capacidad(self):
+        capacidad = self.cleaned_data.get('capacidad')
+        if capacidad <= 0:
+            raise forms.ValidationError("La capacidad debe ser un numero positivo.")
+        return capacidad
+    
+class EventoForm(forms.ModelForm):
+    class Meta:
+        model = Evento
+        fields = '__all__'
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
+            'fecha_inicio': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'fecha_fin': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+    # Validación personalizada para el campo capacidad
+    def clean_capacidad(self):
+        capacidad = self.cleaned_data.get('capacidad')
+        if capacidad <= 0:
+            raise forms.ValidationError("La capacidad debe ser un numero positivo.")
+        return capacidad
+class NotificacionForm(forms.ModelForm):
+    class Meta:
+        model = Notificacion
         fields = '__all__'
         widgets = {
             'nom': forms.TextInput(attrs={
--- ProyectoColegio/app/urls.py
+++ ProyectoColegio/app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-
+from app.views import *
+from django.urls import path
 from app.views.Curso.views import *
+from app.views.Movimiento.views import *
+from app.views.Evento.views import *
+from app.views.Notificacion.views import *
 
 app_name = 'app'
 urlpatterns = [
@@ -12,4 +16,20 @@ urlpatterns = [
     path('curso/editar/<int:pk>/',CursoupdateView.as_view(),name="editar_curso"),
     path('curso/eliminar/<int:pk>/',CursoDeleteView.as_view(),name="eliminar_curso"),
     path('curso/limpiar/',CursoCleandView.as_view(),name="limpiar_curso"),
+    path('movimiento/',MovimientoListView.as_view(),name="index_movimiento"),
+    path('movimiento/crear/',MovimientoCreateView.as_view(),name="crear_movimiento"),
+    path('movimiento/editar/<int:pk>/',MovimientoupdateView.as_view(),name="editar_movimiento"),
+    path('movimiento/eliminar/<int:pk>/',MovimientoDeleteView.as_view(),name="eliminar_movimiento"),
+    path('movimiento/limpiar/',MovimientoCleandView.as_view(),name="limpiar_movimiento"),
+    path('evento/',EventoListView.as_view(),name="index_evento"),
+    path('evento/crear/',EventoCreateView.as_view(),name="crear_evento"),
+    path('evento/editar/<int:pk>/',EventoupdateView.as_view(),name="editar_evento"),
+    path('evento/eliminar/<int:pk>/',EventoDeleteView.as_view(),name="eliminar_evento"),
+    path('evento/limpiar/',EventoCleandView.as_view(),name="limpiar_evento"),
+    path('notificacion/',NotificacionListView.as_view(),name="index_notificacion"),
+    path('notificacion/crear/',NotificacionCreateView.as_view(),name="crear_notificacion"),
+    path('notificacion/editar/<int:pk>/',NotificacionupdateView.as_view(),name="editar_notificacion"),
+    path('notificacion/eliminar/<int:pk>/',NotificacionDeleteView.as_view(),name="eliminar_notificacion"),
+    path('notificacion/limpiar/',NotificacionCleandView.as_view(),name="limpiar_notificacion"),
 ]
+
