d78861095b5a75367aae3000414fc6a7bf354fb4
David <ferchitomonroy@gmail.com>
2026-02-19 13:37:21 -0500

    Login django

---
+++ ProyectoColegio/app/views/login/views.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.contrib import messages
+
+class CreLoginView(LoginView):
+    template_name = 'login/login.html'
+    redirect_authenticated_user = True
+
+    def form_valid(self, form):
+        user = form.get_user()
+
+        # Bloquear usuarios inactivos
+        if not user.estado:
+            form.add_error(None, "El usuario no se encuentra activo")
+            return self.form_invalid(form)  # vuelve a renderizar el formulario con el error
+
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('app:dashboard')
+
+
+
