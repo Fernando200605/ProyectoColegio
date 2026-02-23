1521a655823a614c602de9b5c4513b529c918f4b
Nereo Toro <nereotorom@gmail.com>
2026-02-23 06:56:06 -0500

    Actualizacion Views de Categoria y Marca

--- ProyectoColegio/app/forms.py
+++ ProyectoColegio/app/forms.py
@@ -4,7 +4,7 @@ from app.models import Curso
 from app.models import categoria
 from app.models import Elemento
 from django import forms
-from app.models import marca, tipoelemento, UnidadMedida, Elemento
+from app.models import *
 
 class CursoForm(forms.ModelForm):
     class Meta:
@@ -41,6 +41,13 @@ class marcaForm(forms.ModelForm):
     class Meta:
         model = marca
         fields = '__all__'
+        
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        exist = marca.objects.filter(nombre__iexact=nombre).exclude(pk=self.instance.pk).exists()
+        if exist:
+            raise forms.ValidationError("Ya existe una marca con este nombre.")
+        return nombre
 
 class TipoElementoForm(forms.ModelForm):
     class Meta:
@@ -68,4 +75,45 @@ class CategoriaForm(forms.ModelForm):
     class Meta:
         model = categoria
         fields = ['nombre']
+        
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get('nombre')
+        exist = categoria.objects.filter(nombre__iexact=nombre).exclude(pk=self.instance.pk).exists()
+        if exist:
+            raise forms.ValidationError("Ya existe una categoría con este nombre.")
+        return nombre
+    
+class MovimientoForm(forms.ModelForm):
+    class Meta:
+        model = Movimiento
+        fields = '__all__'
+        widgets = {
+            'nom': forms.TextInput(attrs={
+                'class':'form-control'
+            }),
+            'jornada':forms.TextInput(attrs={
+                'class':'form-control'
+            }),
+            'codigo':forms.TextInput(attrs={
+                'class':'form-control'
+            }),
+            'capacidad':forms.NumberInput(attrs={
+                'class':'form-control'
+            }),
+            'docenteid':forms.Select(attrs={
+                'class':'form-control'
+            })
+        }
+    
+class EventoForm(forms.ModelForm):
+    class Meta:
+        model = Eventos 
+        fields = '__all__'
+        widgets = {
+            'titulo': forms.TextInput(attrs={'class': 'form-control'}),
+            'descripcion': forms.Textarea(attrs={'class': 'form-control'}),
+            'fecha_inicio': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'fecha_fin': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        }
+    # Validación personalizada para el campo capacidad
 
