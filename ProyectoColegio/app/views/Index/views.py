05a24112f10fd6f3d78c2bb223bd4442372e9e8a
David Fernando <ferchitomonroy@gmail.com>
2026-03-19 07:39:25 -0500

    Mejora general en mixins , signals y forms , junto con validaciones en js

--- ProyectoColegio/app/views/Index/views.py
+++ ProyectoColegio/app/views/Index/views.py
@@ -2,10 +2,11 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 from app.models import Usuario
 from django.urls import reverse_lazy
-class DashboardView(LoginRequiredMixin, TemplateView):
+from app.mixins import RolMixin
+class DashboardView(LoginRequiredMixin,RolMixin, TemplateView):
     template_name = 'index/dashboard.html'
     login_url = reverse_lazy('app:login')
-
+    roles_permitidos = ['Administrador', 'Docente']
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
